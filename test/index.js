12964ea53ea054280a9f5a4f1077d6f5cdcfc491
何洋 <abruzzi.hraig@gmail.com>
2014-09-20 16:00:33 +0800

    update: update test case and add chai assert.

--- test/index.js
+++ test/index.js
@@ -1,16 +1,17 @@
 var rework = require('rework');
+var assert = require('chai').assert;
 var distinguish = require('..');
 var read = require('fs').readFileSync;
 
 var input = read(__dirname + '/input.css', 'utf8');
+var expect = read(__dirname + '/expect.css', 'utf8').trim();
 
-describe('distinguish', function() {
-    it('should distinguish color', function() {
+describe('distinguish two css files', function() {
+    it('should equivalent which two css files', function() {
         var css = rework(input)
             .use(distinguish())
             .toString()
             .trim();
-        console.log(css);
-        return true;
+        assert.equal(css, expect);
     });
 });
